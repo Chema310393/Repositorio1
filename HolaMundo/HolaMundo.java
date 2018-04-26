fe1bdb422a862c859ef1e42c8e5bc4d810a94376
Chema <jomaroru7@gmail.com>
2018-04-26 10:47:38 +0200

    Hola mundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,10 @@
+/**
+*Realiza tu primera clase HolaMundo
+*@author José María Romero Ruiz
+*@version 1.0
+*/
+class HolaMundo{
+	public static void main(String[] args){
+		System.out.println("Hola mundo!\nSoy José María Romero Ruiz");
+	}
+}
